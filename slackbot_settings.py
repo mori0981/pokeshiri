fe00ae010ac3ebe6c033611990a7e61536e63618
mori0981 <mori.hcu@gmail.com>
2018-07-05 09:28:16 +0900

    initial commit

---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-192537601204-392918529590-dKgCev7n3UnmZdEfr7gry2py"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "何言ってんだこいつ"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
